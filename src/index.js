ebb37bce5819b2631aeeace8618390632dffe5a1
Baklaikin Kostiantyn <baklaikin@gmail.com>
2021-07-08 21:42:55 +0300

    added promisification

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
 import './sass/main.scss';
+import './task-2';
+import './task-3';
+
+function delay(ms) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(`${ms}`);
+            reject('Uppsss');
+        }, `${ms}`);
+    })
+}
+const logger = time => console.log(`Resolved after ${time}ms`);
+delay(2000).then(logger);
+delay(1000).then(logger);
+delay(1500).then(logger);
